6d57ec5af48792a65662e82debf65b80f37e8b1b
emad947 <134236202+emad947@users.noreply.github.com>
2023-05-22 01:00:49 +0300

    aa

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+player =game.create_sprite(0, 0)
+for i in range (5):
+    for j in range(5):
+        player.set(LedSpriteProperty.Y,i)
+        player.set (LedSpriteProperty.X,j)
+        basic.pause(400)  
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function on_forever() {
-    
-})
+let player = game.createSprite(0, 0)
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        player.set(LedSpriteProperty.Y, i)
+        player.set(LedSpriteProperty.X, j)
+        basic.pause(400)
+    }
+}
